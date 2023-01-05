c5be3329422279ea0dbea3f2451b3403cd890b25
Aswin  Dev SP <40211325+aswindevsp@users.noreply.github.com>
2023-01-05 21:19:30 +0530

    Update infixToPostfix.c

--- infixToPostfix.c
+++ infixToPostfix.c
@@ -1,124 +1,113 @@
 #include <stdio.h>
 #include <string.h>
 
-char stack[100];
-int oStack[100];
-
-int oTop = -1;
-int top = -1;
-
-void push(char ch) {
-	top++;
-	stack[top] = ch;
-}
-
-void oPush(char ch) {
-	oTop++;
-	oStack[oTop] = ch;
-}
-
-char pop() {
-	if(top != -1) {
-		top--;
-		return stack[top+1];	
-	} else {
-		return 0;
-	}
-}
-
-char oPop() {
-	if(top != -1) {
-		oTop--;
-		return oStack[oTop+1];	
-	} else {
-		return 0;
-	}
-}
-
 
 int prec(char ch) {
-	if(ch == '+' || ch == '-') 
-		return 1;
-	else if(ch == '*' || ch == '/') 
-		return 2;
-	else if(ch == '^')
-		return 3;
-	else 
-		return -1;
+    if(ch == '+' || ch == '-') {
+        return 1;
+    } else if(ch == '*' || ch == '/') {
+        return 2;
+    } else if(ch == '^') {
+        return 3;
+    } else {
+        return 0;
+    }
 }
 
-int getInt(char ch) {
-	return (ch - '0');
-}
+int isDigit(char num) {
+    if(num >= '0' && num <= '9' ) {
+        return 1;
+    } 
+    return 0;
+}   
 
-int evaluate(char arr[]) {
-	int i = -1;
-	for(int j=0; j<strlen(arr); j++) {
-		i++;
-		if(isdigit(arr[j]) == 1) {
-			int temp = getInt(arr[i]);
-			oPush(temp);	
-		} else {
-			char op = arr[j];
-			int y = oPop();
-			int x = oPop();
-			switch(op) {
-				case '+':
-					oPush(x + y);
-					break;
-				case '-':
-					oPush(x - y);
-					break;
-				case '*':
-					oPush(x * y);
-					break;	
-				case '/':
-					oPush(x / y);
-					break;	
-			}
-			
-		}
-	}
-	
-	printf("Value = %d\n", oStack[0]);
-	
+int isOperant(char op) {
+    if(op == '-' || op == '+' || op == '*' || op == '/' || op == '^') 
+        return 1;
+    else 
+        return 0;
 }
 
 
+int toInt(char n) {
+    return(n-'0');
+}
+void eval(char exp[]) {
+    int stack[100];
+    int top = -1;
 
-void main() {
-	char str[100];
-	char out[100];
-	int k = 0;
-	printf("Enter a string: ");
-	scanf("%s", str);
+    int op = 0;
 
-	push('(');
-	
-	for(int i=0; i<strlen(str); i++) {
-		if(str[i] == '(') {
-			push(str[i]);
-		} else if(isdigit(str[i]) == 1) {
-			out[k++] = str[i];
-			//printf("%c", str[i]);
-		} else if(prec(str[i]) >= prec(stack[top])) {
-			push(str[i]);		
-		} else if(str[i] == ')') {
-			while(stack[top] != '(') {
-				out[k++] = stack[top];
-				//printf("%c", stack[top]);
-				pop();		
-			}
-			pop();
-		}else if(prec(str[i]) < prec(stack[top])) {
-			//printf("%c", stack[top]);
-			out[k++] = stack[top];
-			pop();
-			push(str[i]);
-		}
-	}
-	out[strlen(str)] = '\0';
-	printf("%s \n", out);
-	evaluate(out);
+    for(int i=0; i < strlen(exp); i++) {
+        if(isDigit(exp[i])) {
+            stack[++top] = toInt(exp[i]);
+        } else {
+            char operation = exp[i];
+            int y = stack[top];
+            int x = stack[--top];
+
+            switch (operation) {
+                case '+':
+                    stack[top] = x + y;     
+                    break;
+                case '-':
+                    stack[top] = x - y;     
+                    break;
+                case '*':
+                    stack[top] = x * y;     
+                    break;
+                case '/':
+                    stack[top] = x / y;     
+                    break; 
+            }
+        }
+    }
+
+    printf("And the answer is: %d", stack[0]);
 }
 
+void infix(char exp[]) {
+    char stack[100];
+    int top = -1;
+
+
+    char op[100];
+    int opIndex = -1;
+    
+    for(int i=0; i<=strlen(exp); i++) {
+        if(isDigit(exp[i]) == 1) {
+            op[++opIndex] = exp[i];
+        } else if(exp[i] == '(') {
+            stack[++top] = exp[i];
+        } else if(exp[i] == ')') {
+            while(stack[top] != '(') {  
+                if(isOperant(stack[top])) {
+                    op[++opIndex] = stack[top];
+                    top--; 
+                } else {
+                    top--;
+                }
+            }
+            top--;
+        } else if(isOperant(exp[i])) {
+            if(prec(exp[i]) > prec(stack[top])) {
+                stack[++top] = exp[i];
+            } else {
+                op[++opIndex] = stack[top];
+                stack[top] = exp[i];
+            }
+        }
+    }
+
+    op[++opIndex] = '\0';
+
+    printf("%s", op);
+    eval(op);
+}
+int main() {
+    char exp[100];
+    scanf("%s", exp);
+
+    infix(exp);
+    // eval("7332+*+");
+}
